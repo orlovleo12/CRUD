5070850f6b5240563696598ca9119eb47b5c1586
orlovleo12 <orlovleo12>
2020-04-10 14:20:10 -0500

    Task 1: JDBC

---
+++ src/main/java/service/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package main.java.service;
+
+
+import main.java.dao.UserDao;
+import main.java.dao.UserDaoJDBCImpl;
+import main.java.model.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    private static UserServiceImpl userServiceImpl = new UserServiceImpl();
+    private UserDao userDao = new UserDaoJDBCImpl();
+
+    public UserServiceImpl() {
+
+    }
+
+    public static UserServiceImpl getInstance() {
+        return userServiceImpl;
+    }
+
+    @Override
+    public void addUser(User user) {
+        userDao.addUser(user);
+    }
+
+    @Override
+    public void deleteUser(int userId) {
+        userDao.deleteUser(userId);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userDao.updateUser(user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    public User getUserById(int userId) {
+        return userDao.getUserById(userId);
+    }
+
+    @Override
+    public User getUserByLogin(String login) {
+        return userDao.getUserByLogin(login);
+    }
+}
---
+++ src/main/java/servlet/ServletEdit.java
@@ -0,0 +1,52 @@
+package main.java.servlet;
+
+import main.java.model.User;
+import main.java.service.UserServiceImpl;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/edit")
+public class ServletEdit extends HttpServlet {
+    private static String LIST_USER = "/listUser.jsp";
+    private static String EDIT = "/edit.jsp";
+    private UserServiceImpl service = UserServiceImpl.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String forward;
+        request.setAttribute("users", service.getAllUsers());
+        String action = request.getParameter("action");
+        if (action.equalsIgnoreCase("edit")) {
+            int userId = Integer.parseInt(request.getParameter("userId"));
+            User user = service.getUserById(userId);
+            forward = EDIT;
+            request.setAttribute("user", user);
+            RequestDispatcher view = request.getRequestDispatcher(forward);
+            view.forward(request, response);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action.equalsIgnoreCase("edit")) {
+            User user = new User();
+            user.setId(Integer.parseInt(request.getParameter("id")));
+            user.setName(request.getParameter("name"));
+            user.setLogin(request.getParameter("login"));
+            user.setPassword(request.getParameter("password"));
+            service.updateUser(user);
+            String forward = LIST_USER;
+
+            request.setAttribute("users", service.getAllUsers());
+            RequestDispatcher view = request.getRequestDispatcher(forward);
+            view.forward(request, response);
+        }
+    }
+}
